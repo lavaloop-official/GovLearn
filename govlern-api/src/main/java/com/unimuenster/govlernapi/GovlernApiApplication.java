54ec63cfc3dd10b2905e4305c98bd15702eebc8b
Jan Laptop <Lichte56@gmail.com>
2023-10-07 13:10:34 +0200

    Init Spring Boot

---
+++ govlern-api/src/main/java/com/unimuenster/govlernapi/GovlernApiApplication.java
@@ -0,0 +1,13 @@
+package com.unimuenster.govlernapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovlernApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovlernApiApplication.class, args);
+	}
+
+}
---
+++ govlern-api/src/test/java/com/unimuenster/govlernapi/GovlernApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimuenster.govlernapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovlernApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
