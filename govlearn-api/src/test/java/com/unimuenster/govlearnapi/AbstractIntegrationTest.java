f9fe48098b07bba2b5e26a6d5e13714d428d03e9
Jan <Lichte56@gmail.com>
2023-10-19 12:33:13 +0200

    Added test container for lightning speed integration tests

---
+++ govlearn-api/src/test/java/com/unimuenster/govlearnapi/AbstractIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.unimuenster.govlearnapi;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpassword");
+
+    @BeforeAll
+    static void beforeAll() {
+        // Start the PostgreSQL container only if not already running
+        if (!postgresContainer.isRunning()) {
+            postgresContainer.start();
+        }
+    }
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        // Pass the PostgreSQL connection properties to Spring Boot
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+}
---
+++ govlearn-api/src/test/java/com/unimuenster/govlearnapi/GovlearnApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.unimuenster.govlearnapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SpringBootTest
+class GovlearnApiApplicationTests extends AbstractIntegrationTest{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
