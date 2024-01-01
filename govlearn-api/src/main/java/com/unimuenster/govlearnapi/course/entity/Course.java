531f5f663533ced992fd9af6d86c3ed779747349
Julian Sibbing <jsibbing@uni-muenster.de>
2024-01-01 20:26:30 +0100

    increased length of image and link column for course entity

--- govlearn-api/src/main/java/com/unimuenster/govlearnapi/course/entity/Course.java
+++ govlearn-api/src/main/java/com/unimuenster/govlearnapi/course/entity/Course.java
@@ -26,7 +26,9 @@ public class Course {
 
     @Column(nullable = false)
     protected String name;
+    @Column(length = 1024)
     protected String image;
+    @Column(length = 1024)
     protected String link;
     @Column(nullable = false)
     protected String description;
