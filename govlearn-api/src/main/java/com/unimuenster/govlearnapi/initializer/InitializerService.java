12f782ebf14eac170a06db29686b551295d1dd13
Jan <Lichte56@gmail.com>
2024-01-02 18:02:33 +0100

    Disabled course scraper insertion

--- govlearn-api/src/main/java/com/unimuenster/govlearnapi/initializer/InitializerService.java
+++ govlearn-api/src/main/java/com/unimuenster/govlearnapi/initializer/InitializerService.java
@@ -71,7 +71,7 @@ public class InitializerService {
         insertUser();
         insertCategories();
         insertCourse();
-        insertMassiveCourseList();
+        //insertMassiveCourseList();
         insertTag();
         addTagsToUsers();
         addTagsToCourses();
