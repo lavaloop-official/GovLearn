35e65f92ca27a46a8bbda40b64bbb69332ee13b6
Jan <Lichte56@gmail.com>
2024-01-14 12:51:16 +0100

    Added unique constraints to bookmarks and completion

--- govlearn-api/src/main/java/com/unimuenster/govlearnapi/bookmark/entity/BookmarkedBy.java
+++ govlearn-api/src/main/java/com/unimuenster/govlearnapi/bookmark/entity/BookmarkedBy.java
@@ -5,22 +5,25 @@ import com.unimuenster.govlearnapi.user.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = { "bookmarkee_id", "course_id" })
+}
+)
 public class BookmarkedBy {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 
-    @ManyToOne()
+    @ManyToOne(optional = false)
     @ToString.Exclude
     private UserEntity bookmarkee;
 
-    @ManyToOne()
+    @ManyToOne(optional = false)
     @ToString.Exclude
     private Course course;
 }
--- govlearn-api/src/main/java/com/unimuenster/govlearnapi/course/entity/CourseCompletion.java
+++ govlearn-api/src/main/java/com/unimuenster/govlearnapi/course/entity/CourseCompletion.java
@@ -8,16 +8,20 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = { "completee_id", "course_id" })
+}
+)
 public class CourseCompletion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 
-    @ManyToOne()
+    @ManyToOne(optional = false)
     @ToString.Exclude
     private UserEntity completee;
 
-    @ManyToOne()
+    @ManyToOne(optional = false)
     @ToString.Exclude
     private Course course;
 }
